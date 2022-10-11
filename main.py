5e60e2fb5a1f1d1b35e60974529e389d14b1df1c
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 03:38:17 +0000

    7-5

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-
 import sys
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
 if len(num1) > len(num2):
-	print ('False')
-	sys.exit(0)
+    print ('False')
+    sys.exit(0)
 count = 0
 for i in range(len(num2)):
-	if num2[i-1] == num1[0]:
-		for j in range(len(num1)):
-			if num2[i+j-2] == num1[j-1]:
-				count += 1
-			if count == len(num1):
-				print(True)
-				break
-			else: print(False)
-	else: print(False)
-
-
-
+        if num2[i] == num1[0]:
+            for j in range(len(num1)):
+                if num2[i+j] == num1[j]:
+                    count += 1
+if count == len(num1):
+    print(True)
+else: print(False)
 # ******************************
 # Make your Code
 # ******************************
