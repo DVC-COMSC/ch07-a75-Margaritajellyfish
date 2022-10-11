5f0d3fa0aa2646cef6c094a03e90ad8d1593d18b
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 02:03:18 +0000

    7-5

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for i in range(len(num2)):
 			if num2[i+j-2] == num1[j-1]:
 				count += 1
 			if count == len(num1):
-				printa(True)
+				print(True)
 				break
 			else: print(False)
 	else: print(False)
